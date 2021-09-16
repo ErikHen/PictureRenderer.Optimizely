2d1c3135707d989cb9b7a7f83fd19139f29b069d
Erik Henningson <ErikHen@users.noreply.github.com>
2021-09-16 13:28:47 +0200

    Add project files.

---
+++ PictureHelper.cs
@@ -0,0 +1,42 @@
+﻿using EPiServer;
+using EPiServer.Core;
+using EPiServer.ServiceLocation;
+using EPiServer.Web.Routing;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using PictureRenderer.Profiles;
+using PictureRenderer;
+
+namespace PictureRenderer.Optimizely
+{
+    public static class PictureHelper
+    {
+        public static HtmlString Picture(this IHtmlHelper helper, ContentReference imageReference, PictureProfile profile, LazyLoading lazyLoading)
+        {
+            return Picture(helper, imageReference, profile, string.Empty, lazyLoading);
+        }
+
+        public static HtmlString Picture(this IHtmlHelper helper, ContentReference imageReference, PictureProfile profile, string altText = "", LazyLoading lazyLoading = LazyLoading.Browser)
+        {
+            if (imageReference == null)
+            {
+                return new HtmlString(string.Empty);
+            }
+
+            if (profile.GetDataFromImage)
+            {
+                var image = ServiceLocator.Current.GetInstance<IContentLoader>().Get<IContent>(imageReference);
+                if (string.IsNullOrEmpty(altText) && image?.Property["AltText"]?.Value != null)
+                {
+                    altText = image.Property["AltText"].ToString();
+                }
+                //TODO: focal point   (double x, double y) focalPoint = default;
+            }
+
+
+            var imageUrl = new UrlBuilder(ServiceLocator.Current.GetInstance<UrlResolver>().GetUrl(imageReference));
+
+            return new HtmlString(PictureRenderer.Picture.Render(imageUrl.ToString(), profile, altText, lazyLoading));
+        }
+    }
+}
---
+++ PictureProfile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PictureRenderer.Profiles;
+
+namespace PictureRenderer.Optimizely
+{
+    public class PictureProfile : ImageSharpProfile
+    {
+        /// <summary>
+        /// Set to false for a potential slight performance gain. But it will not be possible to get alt text or focal point from image. 
+        /// </summary>
+        public bool GetDataFromImage { get; set; }
+
+        public PictureProfile()
+        {
+            GetDataFromImage = true;
+        }
+    }
+}
